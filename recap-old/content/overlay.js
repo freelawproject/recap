dfe329fcf609285b032fcc344229405a4368c4cc
Stephen Schultze <steve@stephen-schultzes-macbook-pro-15.local>
2009-04-05 18:54:43 -0400

    Major refactoring to support tab-specific status icon.  Changed pdf_header functionality to be a javascript auto-check instead of page modification.  Fixed crash on OSX when Growl not installed.

--- recap-old/content/overlay.js
+++ recap-old/content/overlay.js
@@ -3,18 +3,37 @@ var recap = {
     onLoad: function() {
 	// initialization code
 	this.initialized = true;
-	
-	//var recapPanel = document.getElementById('recap-panel');
-
-	var recapService = Cc["@cs.princeton.edu/recap;1"]
-	                    .getService().wrappedJSObject;
-	
-	gBrowser.addProgressListener(recapService.getContentListener(),
-				     Ci.nsIWebProgress.NOTIFY_LOCATION);
+				     
+	var container = gBrowser.tabContainer;
+	container.addEventListener("TabSelect", exampleTabSelected, false);
 
     },
     
 };
 
+
 window.addEventListener("load", function(e) { recap.onLoad(e); }, false);
+
+
+function exampleTabSelected(event)
+{
+  updateStatusIcon(); 
+}
+
+function updateStatusIcon() {
+  //log("current domain is PACER domain? " + isPACERHost(gBrowser.selectedBrowser.contentDocument.domain));
+  
+  var statusIcon = document.getElementById("recap-panel-image");
+  
+  	if (isPACERHost(gBrowser.selectedBrowser.contentDocument.domain) && havePACERCookie()) {
+	    statusIcon.src = ICON_LOGGED_IN;
+	    statusIcon.tooltipText = "You are logged into PACER.";
+
+	} else {
+	    statusIcon.src= ICON_LOGGED_OUT;
+	    statusIcon.tooltipText = "You are logged out of PACER.";
+	}
+}
+
+
 
