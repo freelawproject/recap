33bbdccc7a87763b40d05b9bfb1266d0b0e7686e
Stephen Schultze <steve@stephen-schultzes-macbook-pro-15.local>
2009-05-20 22:31:14 -0400

    we now have a better preferences pane.  I also added a menu item under "Tools" to access it.  Also, the toolbar [R] opens prefs instead of going to a URL.

--- recap-firefox/content/recapModal.js
+++ recap-firefox/content/recapModal.js
@@ -1,12 +1,6 @@
-
 function addModal(num) {
     $('#recapdialog' + num).jqm();
     $('#recapdialog' + num).jqmShow();
 }
 
-var pdfHeadersCheckbox;
-pdfHeadersCheckbox = document.getElementsByName("pdf_header");
-if (pdfHeadersCheckbox.length > 0) {	
-	pdfHeadersCheckbox[0].checked=true;
-}
 
