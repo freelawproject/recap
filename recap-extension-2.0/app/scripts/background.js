5a11da348c631234eb505615a63c11cf88c87c20
Nick Semenkovich <semenko@alum.mit.edu>
2015-01-15 12:26:04 -0600

    Clone background.

--- recap-extension-2.0/app/scripts/background.js
+++ recap-extension-2.0/app/scripts/background.js
@@ -8,4 +8,22 @@ chrome.tabs.onUpdated.addListener(function (tabId) {
     chrome.pageAction.show(tabId);
 });
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+console.log('background.js Event Page for Page Action loaded');
+
+
+// Set defaults on first instantiation
+chrome.storage.sync.get('options', function (items) {
+  if (!items.options) {
+    chrome.storage.sync.set({options: {
+      recap_link_popups: true,
+      status_notifications: true,
+      upload_notifications: true
+    }});
+  }
+});
+
+// Make services callable from content scripts.
+exportInstance(Notifier);
+exportInstance(ToolbarButton);
+exportInstance(Pacer);
+exportInstance(Recap);
