183b7c459e3fa5e378f05317c0be594c185e6c6a
Nick Semenkovich <semenko@alum.mit.edu>
2015-01-15 12:19:22 -0600

    Stub create options page from previous extension.

--- recap-extension-2.0/app/scripts/options.js
+++ recap-extension-2.0/app/scripts/options.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+console.log('Options page js loaded.');
+
+var inputs = document.getElementsByTagName('input');
+
+function load_options() {
+    chrome.storage.sync.get('options', function (items) {
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].checked = items.options[inputs[i].id];
+        }
+    });
+}
+
+function save_options() {
+    var options = {};
+    for (var i = 0; i < inputs.length; i++) {
+        options[inputs[i].id] = inputs[i].checked;
+    }
+    chrome.storage.sync.set({options: options});
+}
+
+load_options();
+for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('change', save_options);
+}
