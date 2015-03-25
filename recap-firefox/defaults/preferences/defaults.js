508fbe4c3039b1d80efe95a17a083e2733f2b501
Michael Lissner <mlissner@michaeljaylissner.com>
2015-03-25 16:14:06 -0700

    Adds preference for team name to the front end only.

--- recap-firefox/defaults/preferences/defaults.js
+++ recap-firefox/defaults/preferences/defaults.js
@@ -1,7 +1,8 @@
-pref("extensions.recap.cache_time_ms", 86400000); //24*60*60*1000
+pref("extensions.recap.cache_time_ms", 86400000); //24*60*60*1000 = 1 day
 pref("extensions.recap.pretty_filenames", true);
 pref("extensions.recap.pretty_filenames_choice", "pretty_filenames_IAFilename");
 pref("extensions.recap.auto_check_pdf_headers", true);
 pref("extensions.recap.display_notifications", true);
 pref("extensions.recap.temp_disable", false);
 pref("extensions.recap.has_been_initialized", false);
+pref("extensions.recap.team_name", "");
