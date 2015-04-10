74ec328f8c2f43b7ab00ef4a10ade639019de215
Michael Lissner <mlissner@michaeljaylissner.com>
2015-04-10 12:46:10 -0700

    Bad merge. Fixes issue with urls

--- recap-server/uploads/urls.py
+++ recap-server/uploads/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import patterns
+from django.conf.urls.defaults import *
 
-urlpatterns = patterns(
-    'uploads.views',
-    (r'^$', 'index'),
-    (r'^upload/$', 'upload'),
-    (r'^query/$', 'query'),
-    (r'^query_cases/$', 'query_cases'),
-    (r'^adddocmeta/$', 'adddocmeta'),
-    (r'^lock/$', 'lock'),
-    (r'^unlock/$', 'unlock'),
-    (r'^querylocks/$', 'querylocks'),
-    (r'^get_updated_cases/$', 'get_updated_cases'),
-    (r'^heartbeat/$', 'heartbeat'),
-    (r'^load/$', 'load'),
+urlpatterns = patterns('uploads.views',
+                       (r'^$', 'index'),
+                       (r'^upload/$', 'upload'),
+                       (r'^query/$', 'query'),
+                       (r'^query_cases/$', 'query_cases'),
+                       (r'^adddocmeta/$', 'adddocmeta'),
+                       (r'^lock/$', 'lock'),
+                       (r'^unlock/$', 'unlock'),
+                       (r'^querylocks/$', 'querylocks'),
+                       (r'^get_updated_cases/$', 'get_updated_cases'),
+                       (r'^heartbeat/$', 'heartbeat'),
+                       (r'^load/$', 'load'),
+
 )
--- recap-server/urls.py
+++ recap-server/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings as config
 
-urlpatterns = patterns('',
-                       (r'^%s' % config.SERVER_BASEDIR, 
-                        include('recap-server.uploads.urls')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+urlpatterns = patterns(
+    '',
+    (r'^%s' % config.SERVER_BASEDIR, include('recap-server.uploads.urls')),
 )
